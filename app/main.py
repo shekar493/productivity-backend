8975537b07d1cec36a42c4af5c081c2228b27112
Raja Shekar <shekarraja2288@gmail.com>
2026-06-04 19:34:31 +0530

    new commit

--- app/main.py
+++ app/main.py
@@ -24,6 +24,9 @@ app.add_middleware(
         "http://127.0.0.1:8080",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+
+        # Render Frontend URL
+        "https://productivity-app.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
